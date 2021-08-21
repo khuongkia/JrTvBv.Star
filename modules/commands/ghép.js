31b198d3e7f6190621c27b8f685d44cd2855a1b4
JRT <88273015+JRT-Music@users.noreply.github.com>
2021-08-21 10:46:03 +0700

    Update ghép.js

--- modules/commands/ghép.js
+++ modules/commands/ghép.js
@@ -2,45 +2,45 @@ module.exports.config = {
   name: "ghép",
   version: "1.0.0", 
   hasPermssion: 0,
-  credits: "Hungcho edit by Hungdz30cm",
-  description: "Ghep doi ngau nhien",
-  commandCategory: "random-img", 
+  credits: "JRT",
+  description: "Ghép đôi",
+  commandCategory: "Game", 
   usages: "ghép", 
-  cooldowns: 0,
+  cooldowns: 10,
   dependencies: [] 
 };
-module.exports.run = async function({ api, event, args, Users, Threads, Currencies }) {
+module.exports.run = async function({ api, event, Users, Currencies }) {
         const axios = global.nodemodule["axios"];
         const fs = global.nodemodule["fs-extra"];
         var data = await Currencies.getData(event.senderID);
         var money = data.money
-        if(money < 500) api.sendMessage("Bạn cần 500 đô cho 1 lần ghép hãy tích cực làm việc hoặc xin admin bot!\n🤑Có làm mới có ăn🤑",event.threadID,event.messageID)
+        if(money = 0, money >0) api.sendMessage("⚡️Nghèo quá nên tôi không biết ghép cho ai nhé!")
         else {
-        var tl = ['21%', '67%', '19%', '37%', '17%', '96%', '52%', '62%', '76%', '83%', '100%', '99%', "0%", "48%"];
-        var tle = tl[Math.floor(Math.random() * tl.length)];
-        let dataa = await api.getUserInfo(event.senderID);
-        let namee = await dataa[event.senderID].name
+        var tle = Math.floor(Math.random() * 101);
+
+        var namee = (await Users.getData(event.senderID)).name
+
         let loz = await api.getThreadInfo(event.threadID);
         var emoji = loz.participantIDs;
         var id = emoji[Math.floor(Math.random() * emoji.length)];
-        let data = await api.getUserInfo(id);
-        let name = await data[id].name
+        /*let data = await api.getUserInfo(id);*/
+        
+        var name = (await Users.getData(id)).name
+
+        /*var sex = await data[id].gender;*/
         var arraytag = [];
                 arraytag.push({id: event.senderID, tag: namee});
                 arraytag.push({id: id, tag: name});
-        api.changeNickname(`Con vợ của ${name}`, event.threadID, event.senderID);
-        api.changeNickname(`Thằng chồng của ${namee}`, event.threadID, id);
-        var sex = await data[id].gender;
-        var gender = sex == 2 ? "Nam🧑" : sex == 1 ? "Nữ👩‍🦰" : "Trần Đức Bo";
-        Currencies.setData(event.senderID, options = {money: money - 500})
-        let Avatar = (await axios.get( `https://graph.facebook.com/${id}/picture?height=720&width=720&access_token=170440784240186|bc82258eaaf93ee5b9f577a8d401bfc9`, { responseType: "arraybuffer" } )).data;
+
+        /*var gender = sex == 2 ? "Nam🧑" : sex == 1 ? "Nữ👩‍🦰" : "mèo méo meo mèo meowww";*/
+        let Avatar = (await axios.get( `https://le31.glitch.me/avt?q=${id}`, { responseType: "arraybuffer" } )).data;
             fs.writeFileSync( __dirname + "/cache/avt.png", Buffer.from(Avatar, "utf-8") );
-        let Avatar2 = (await axios.get( `https://graph.facebook.com/${event.senderID}/picture?height=720&width=720&access_token=170440784240186|bc82258eaaf93ee5b9f577a8d401bfc9`, { responseType: "arraybuffer" } )).data;
+        let Avatar2 = (await axios.get( `https://le31.glitch.me/avt?q=${event.senderID}`, { responseType: "arraybuffer" } )).data;
             fs.writeFileSync( __dirname + "/cache/avt2.png", Buffer.from(Avatar2, "utf-8") );
         var imglove = [];
               imglove.push(fs.createReadStream(__dirname + "/cache/avt.png"));
               imglove.push(fs.createReadStream(__dirname + "/cache/avt2.png"));
-        var msg = {body: `Hoàn thanh ghép đôi bạn đã mất 500 đô!\nNgười ghép đôi với bạn có giới tính: ${gender}\nTỉ lệ hợp đôi: ${tle}\n`+namee+" "+"💓"+" "+name, mentions: arraytag, attachment: imglove}
+        var msg = {body: `⚡️Ghép đôi thành công!\n⚡️Tỉ lệ hợp đôi: ${tle}%\n`+namee+" "+"💓"+" "+name, mentions: arraytag, attachment: imglove}
         return api.sendMessage(msg, event.threadID, event.messageID)
       }
 }
